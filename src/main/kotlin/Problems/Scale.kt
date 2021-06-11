6c6e17dab833edf5968a7c6ecfefde54d49281c2
hyejineee <dev.hyejin04@gmail.com>
2021-06-11 18:10:08 +0900

    Problem solving

--- src/main/kotlin/Problems/Scale.kt
+++ src/main/kotlin/Problems/Scale.kt
@@ -1,4 +1,23 @@
 package Problems
 
-fun Scale() {
+fun scale(scale: IntArray): String {
+    var sum = 0
+
+    for (i in 0..scale.size - 2) {
+        val step = scale[i] - scale[i + 1]
+
+        if (step != -1 && step != 1) {
+            return "mixed"
+        }
+
+        sum += step
+
+    }
+
+    return if (sum > 0) {
+        "descending"
+    } else {
+        "ascending"
+    }
+
 }
