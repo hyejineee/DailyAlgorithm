c56a3f588fa6a381b460d3d984c4d4c30dfd686c
hyejineee <dev.hyejin04@gmail.com>
2021-06-21 15:32:59 +0900

    scale 코드 정리

--- src/main/kotlin/Problems/Scale.kt
+++ src/main/kotlin/Problems/Scale.kt
@@ -3,7 +3,6 @@ package Problems
 fun scale(scale: IntArray): String {
 
     val sum = (0..scale.size - 2).foldIndexed(0) { value: Int, acc: Int, index: Int ->
-        println("${value} : ${index} : ${acc}")
         val step: Int = scale[index] - scale[index + 1]
 
         if (step != -1 && step != 1) {
