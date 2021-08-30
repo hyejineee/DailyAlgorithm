b19234940290e2e8d6688334845d8752214ade2e
hyejineee <dev.hyejin04@gmail.com>
2021-08-30 22:36:58 +0900

    프로그래머스- 크레인 인형뽑기 문제풀이

---
+++ src/main/kotlin/Programmers/pickUpTheDollOfCrane.kt
@@ -0,0 +1,38 @@
+package Programmers
+
+import java.util.*
+
+fun pickUpTheDollOfCrane(board: Array<IntArray>, moves: IntArray): Int {
+
+    val queueBoard = List<LinkedList<Int>>(board.size) {
+        LinkedList()
+    }
+
+    for (i in board.indices) {
+        for (j in board[i].indices) {
+            if (board[i][j] == 0) continue
+            queueBoard[j].push(board[i][j])
+        }
+    }
+
+    val basket = Stack<Int>()
+
+    var popCount = 0
+
+    for (i in moves) {
+
+        if (queueBoard[i - 1].isEmpty()) continue
+
+        val doll = queueBoard[i - 1].pollLast()
+        val prevDoll = if (basket.isNotEmpty()) basket.peek() else -1
+
+        if (doll == prevDoll) {
+            basket.pop()
+            popCount += 2
+            continue
+        }
+
+        basket.push(doll)
+    }
+    return popCount
+}
