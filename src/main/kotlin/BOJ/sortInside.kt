338cb94d86d0f92e371b06e700042d5c39ced223
hyejineee <dev.hyejin04@gmail.com>
2021-10-05 14:05:42 +0900

    백준 소트인사이드 문제풀이

---
+++ src/main/kotlin/BOJ/sortInside.kt
@@ -0,0 +1,19 @@
+package BOJ
+
+
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+
+fun main() {
+    with(BufferedReader(InputStreamReader(System.`in`))) {
+        val bw = BufferedWriter(OutputStreamWriter(System.out))
+        
+
+        bw.write(readLine().toCharArray().sortedDescending().joinToString(""))
+
+        bw.flush()
+        bw.close()
+    }
+}
