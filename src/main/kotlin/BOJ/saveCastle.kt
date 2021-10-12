5fe087908746f54786f95c8d44292cb94a8c8e7a
hyejineee <dev.hyejin04@gmail.com>
2021-10-12 20:31:18 +0900

    백준 - 성지키기

---
+++ src/main/kotlin/BOJ/saveCastle.kt
@@ -0,0 +1,30 @@
+package BOJ
+
+
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+
+fun main() {
+    with(BufferedReader(InputStreamReader(System.`in`))) {
+        val bw = BufferedWriter(OutputStreamWriter(System.out))
+
+        val (n, m) = readLine().split(" ").map { it.toInt() }
+
+        val c = MutableList(n) {
+            readLine().toCharArray()
+        }
+
+        val r = MutableList(m) { i ->
+            MutableList(n) { j ->
+                c[j][i]
+            }
+        }
+
+        bw.write("${maxOf(c.filter { 'X' !in it }.count(), r.filter { 'X' !in it }.count())}")
+
+        bw.flush()
+        bw.close()
+    }
+}
