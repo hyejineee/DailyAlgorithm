62c07fb444e6ac306bee2089f1b1074a7a0a4397
hyejineee <dev.hyejin04@gmail.com>
2021-10-27 10:12:45 +0900

    동적 프로그래밍 핵심유형 다시 풀기
    백준 - LCS, 기타리스트, 가장 높은 탑 쌓기 문제풀이

---
+++ src/main/kotlin/BOJ/LCS3.kt
@@ -0,0 +1,36 @@
+package BOJ
+
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+
+fun main() {
+    with(BufferedReader(InputStreamReader(System.`in`))) {
+        val bw = BufferedWriter(OutputStreamWriter(System.out))
+
+        val arr1 = readLine().toCharArray().toMutableList().apply {  add(0,'-')}
+        val arr2 = readLine().toCharArray().toMutableList().apply { add(0, '-') }
+        val dp = MutableList(arr1.size){
+            MutableList(arr2.size){0}
+        }
+
+        for (i in 1 until arr1.size){
+            for(j in 1 until arr2.size) {
+                dp[i][j] = if(arr1[i] == arr2[j]){
+                    dp[i-1][j-1] + 1
+                }else{
+                    maxOf(dp[i-1][j], dp[i][j-1])
+                }
+            }
+        }
+
+        println(dp.flatten().maxOrNull())
+
+
+
+
+        bw.flush()
+        bw.close()
+    }
+}
