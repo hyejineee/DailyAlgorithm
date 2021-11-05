96849a1cef214e31b9ce0922a591ddfedbb5dfc6
hyejineee <dev.hyejin04@gmail.com>
2021-11-05 10:45:43 +0900

    패스트 캠퍼스 part4 chapter2 문제풀이
    백준 - 행복, 수빈이와 수열, 이름궁합 테스트, 보너스 점수

---
+++ src/main/kotlin/BOJ/nameMatchTest.kt
@@ -0,0 +1,84 @@
+package BOJ
+
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
+    val (n, m) = readLine().split(" ").map { it.toInt() }
+    val (name1, name2) = readLine().split(" ")
+
+    val map = mutableMapOf<Char, Int>().apply {
+        this['A'] = 3
+        this['B'] = 2
+        this['C'] = 1
+        this['D'] = 2
+        this['E'] = 4
+        this['F'] = 3
+        this['G'] = 1
+        this['H'] = 3
+        this['I'] = 1
+        this['J'] = 1
+        this['K'] = 3
+        this['L'] = 1
+        this['M'] = 3
+        this['N'] = 2
+        this['O'] = 1
+        this['P'] = 2
+        this['Q'] = 2
+        this['R'] = 2
+        this['S'] = 1
+        this['T'] = 2
+        this['U'] = 1
+        this['V'] = 1
+        this['W'] = 1
+        this['X'] = 2
+        this['Y'] = 2
+        this['Z'] = 1
+    }
+
+    val matchName = if(n > m){
+        name1.foldIndexed(""){ index, acc, c ->
+            if(index in name2.indices){
+                acc+"$c${name2[index]}"
+            }else{
+                acc+"$c"
+            }
+        }
+
+    }else{
+        name2.foldIndexed(""){ index, acc, c ->
+            if(index in name1.indices){
+                acc+"${name1[index]}$c"
+            }else{
+                acc+"$c"
+            }
+        }
+    }
+
+    var numbers = matchName.map{ map[it]!!}.toMutableList()
+
+    while (numbers.size >2){
+        numbers = calculate(numbers)
+    }
+
+
+
+    if(numbers.first() == 0){
+        println("${numbers.last()}%")
+    }else{
+        val percent = numbers.joinToString("")
+        println("${percent}%")
+    }
+}
+
+private fun calculate(numbers: MutableList<Int>): MutableList<Int> {
+    val newNumbers = mutableListOf<Int>()
+    for(i in 0 until numbers.size-1){
+        var next = numbers[i] + numbers[i+1]
+        if( next>=10){
+            next %= 10
+        }
+        newNumbers.add(next)
+    }
+    return newNumbers
+}
