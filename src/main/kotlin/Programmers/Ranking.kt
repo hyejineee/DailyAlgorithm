0bfe605c8c67b20ff84182d116caeae53ae753d2
hyejineee <dev.hyejin04@gmail.com>
2022-01-28 15:05:16 +0900

    플로이드-워셜 알고리즘 기본형태

--- src/main/kotlin/Programmers/Ranking.kt
+++ src/main/kotlin/Programmers/Ranking.kt
@@ -1,6 +1,8 @@
 package Programmers
 
 import java.util.*
+import kotlin.math.cos
+import kotlin.math.min
 
 fun ranking(n: Int, results: Array<IntArray>): Int {
     val board = Array(n) {CharArray(n){'x'} }
@@ -20,5 +22,41 @@ fun ranking(n: Int, results: Array<IntArray>): Int {
             }
         }
     }
+    floyd()
     return  board.filter { arr -> arr.count { it =='x'} ==1 }.size
+}
+
+fun floyd(){
+
+    val inf = 10000000
+    val nodeCount = 5
+    val graph = arrayOf(
+        intArrayOf(3,4,7),
+        intArrayOf(3,2,2),
+        intArrayOf(2,1,5),
+        intArrayOf(1,4,9),
+        intArrayOf(4,5,2),
+        intArrayOf(1,5,1)
+    )
+
+
+    val distances = Array(5+1){ i->
+        IntArray(5+1){ j ->
+            if(i==j) 0 else inf
+        }
+    }
+
+    graph.forEach {
+        val (a, b, cost) = it
+        distances[a][b] = cost
+        distances[b][a] = cost
+    }
+
+    for (k in 1..nodeCount){
+        for(i in 1..nodeCount){
+            for (j in 1..nodeCount){
+                distances[i][j] = min(distances[i][j],  distances[i][k] + distances[k][j])
+            }
+        }
+    }
 }
