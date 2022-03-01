139e953cb1a67175005c7a633a7f0a24ab9ff572
hyejineee <dev.hyejin04@gmail.com>
2022-03-01 13:17:08 +0900

    이것이 코딩테스트다 - 미로찾기

--- src/main/kotlin/thisIsCodingTest/BFSDFS.kt
+++ src/main/kotlin/thisIsCodingTest/BFSDFS.kt
@@ -1,5 +1,8 @@
 package thisIsCodingTest
 
+import Programmers.convert
+import java.util.*
+
 class BFSDFS {
 
     fun freezeDrinks(board: Array<IntArray>): Int {
@@ -39,5 +42,37 @@ class BFSDFS {
             return true
         }
         return false
+    }
+
+    fun mazeEscape(n:Int, m:Int, maze:Array<IntArray>):Int{
+
+        val dx = intArrayOf(1,0,-1,0)
+        val dy = intArrayOf(0,1,0,-1)
+
+        val needVisit = Stack<IntArray>()
+        needVisit.add(intArrayOf(0,0,1))
+
+        while (needVisit.isNotEmpty()){
+            maze.forEach {
+                println(it.toList())
+            }
+            println("=======")
+            val (y,x, count) = needVisit.pop()
+
+            for(i in 0..3) {
+                val nx = x + dx[i]
+                val ny = y + dy[i]
+
+                if((ny in 0 until n && nx in 0 until m).not()) continue
+                if(maze[ny][nx] == 0 ) continue
+
+                if(maze[ny][nx] ==1){
+                    maze[ny][nx] = count+1
+                    needVisit.add(intArrayOf(ny,nx, count+1))
+                }
+            }
+        }
+
+        return maze[n-1][m-1]
     }
 }
