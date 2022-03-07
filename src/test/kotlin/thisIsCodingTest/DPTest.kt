eba765b3f7d22ab84d33195f40fab884a25af566
hyejineee <dev.hyejin04@gmail.com>
2022-03-07 17:47:26 +0900

    이것이 코딩테스트다 - 다이나믹 프로그래밍

--- src/test/kotlin/thisIsCodingTest/DPTest.kt
+++ src/test/kotlin/thisIsCodingTest/DPTest.kt
@@ -1,4 +1,20 @@
 package thisIsCodingTest
 
+import assertk.assertThat
+import assertk.assertions.isEqualTo
+import org.junit.Test
 
-internal class DPTest
+
+internal class DPTest{
+    val methods = DP()
+
+    @Test
+    fun make1Test(){
+        assertThat(methods.make1(26)).isEqualTo(3)
+    }
+
+    @Test
+    fun antWarriorTest(){
+        assertThat(methods.antWarrior(intArrayOf(1,3,1,5))).isEqualTo(8)
+    }
+}
