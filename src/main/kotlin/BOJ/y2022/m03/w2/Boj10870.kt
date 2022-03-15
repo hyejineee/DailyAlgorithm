68e5e8bb5c4802f7e306c134f8954b793c01a322
hyejineee <dev.hyejin04@gmail.com>
2022-03-15 15:05:33 +0900

    백준 - 피보나치

---
+++ src/main/kotlin/BOJ/y2022/m03/w2/Boj10870.kt
@@ -0,0 +1,26 @@
+package BOJ
+
+
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+
+fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
+    val n = readLine().toInt()
+
+    if(n == 0){
+        println(0)
+        return@with
+    }
+
+    val dp = IntArray(n + 1) { 0 }
+    dp[0] = 0
+    dp[1] = 1
+
+    for (i in 2..n) {
+        dp[i] = dp[i - 1] + dp[i - 2]
+    }
+
+    println(dp[n])
+}
+
