72e285a048666c0d9a0bf5fff4505b451794b639
hyejineee <dev.hyejin04@gmail.com>
2022-03-29 16:38:46 +0900

    백준 - 파도반 수열

---
+++ src/main/kotlin/BOJ/y2022/m03/w5/Boj9461.kt
@@ -0,0 +1,25 @@
+package BOJ.y2022.m03.w5
+
+
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+fun main() {
+    with(BufferedReader(InputStreamReader(System.`in`))) {
+        val tc = readLine().toInt()
+        val seed = longArrayOf(0, 1, 1, 1, 2, 2, 3, 4, 5, 7, 9)
+
+        for (t in 0 until tc) {
+            val n = readLine().toInt()
+            val dp = if (n < 11) LongArray(11) { i -> seed[i] } else LongArray(n + 1) { i ->
+                if (i <= 10) seed[i] else 0
+            }
+
+            for (i in 11..n) {
+                dp[i] = dp[i - 1] + dp[i - 5]
+            }
+
+            println(dp[n])
+        }
+    }
+}
