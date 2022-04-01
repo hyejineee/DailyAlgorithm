26aa8b075af23a899bf67e4b5cf64563256c1941
hyejineee <dev.hyejin04@gmail.com>
2022-04-01 11:56:34 +0900

    백준 - 가장 긴 증가하는 부분 수열

---
+++ src/main/kotlin/BOJ/y2022/m04/w1/Boj11053.kt
@@ -0,0 +1,31 @@
+package BOJ
+
+
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+
+fun main() {
+    with(BufferedReader(InputStreamReader(System.`in`))) {
+        val bw = BufferedWriter(OutputStreamWriter(System.out))
+
+        val n = readLine().toInt()
+        val arr = readLine().split(" ").map { it.toInt() }
+        val dp = IntArray(n){1}
+
+        for(i in 0 until n){
+            for( j in 0..i){
+                if(arr[j] < arr[i]){
+                    dp[i]= maxOf(dp[i], dp[j] + 1)
+                }
+            }
+        }
+
+        bw.write((dp.maxOrNull()?:-1).toString())
+
+        bw.flush()
+        bw.close()
+    }
+}
+
