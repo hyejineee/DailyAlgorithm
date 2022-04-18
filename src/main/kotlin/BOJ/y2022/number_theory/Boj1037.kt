0d5abd228a353ae913091dbd36b79a8b991284ef
hyejineee <dev.hyejin04@gmail.com>
2022-04-18 15:32:49 +0900

    백준 - 최대공약수와 최대공배수

--- src/main/kotlin/BOJ/y2022/number_theory/Boj1037.kt
+++ src/main/kotlin/BOJ/y2022/number_theory/Boj1037.kt
@@ -7,9 +7,13 @@ import java.io.InputStreamReader
 
 fun main() {
     with(BufferedReader(InputStreamReader(System.`in`))) {
-        val n = readLine().toInt()
-        val arr = readLine().split(" ").map { it.toInt() }.sorted()
-        println(arr.first() * arr.last())
+        val (n,m) = readLine().split(" ").map { it.toInt() }
+        val gcd = gcd(n,m)
+        println(gcd)
+        println((n*m)/gcd)
     }
 }
+
+private fun gcd(n1: Int, n2: Int): Int = if (n2 == 0) n1 else gcd(n2, n1 % n2)
+
 
