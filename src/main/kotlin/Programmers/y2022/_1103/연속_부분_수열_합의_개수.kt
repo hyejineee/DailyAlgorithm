592c12f898a0f3531c261d9ce8b7f81830a0aa90
hyejineee <dev.hyejin04@gmail.com>
2022-11-04 19:30:44 +0900

    프로그래머스 - 연속 부분 수열 합의 개수

--- src/main/kotlin/Programmers/y2022/_1103/연속_부분_수열_합의_개수.kt
+++ src/main/kotlin/Programmers/y2022/_1103/연속_부분_수열_합의_개수.kt
@@ -6,7 +6,7 @@ import kotlin.math.max
 data class Node(val head: Boolean, val value: Int)
 
 fun `연속 부분 수열 합의 개수`(elements: IntArray): Int {
-    val arr = LinkedList(elements.mapIndexed { index, i -> Node(head = index == 0, value = i) })
+    val arr = elements.mapIndexed { index, i -> Node(head = index == 0, value = i) }.toMutableList()
     val result = mutableSetOf<Int>()
 
     for(i in 1 .. elements.size){
@@ -17,12 +17,24 @@ fun `연속 부분 수열 합의 개수`(elements: IntArray): Int {
             }
 
             result.add(sum)
-            arr.offerLast(arr.pollFirst())
-            println(arr)
+            arr.add(arr.removeAt(0))
 
-            if(arr.first.head) break
+            if(arr.first().head) break
         }
     }
 
     return result.size
 }
+
+//class Solution {
+//    fun solution(elements: IntArray): Int {
+//        val doubling = elements+elements
+//        val result = HashSet<Int>()
+//        for (i in 1..elements.size) {
+//            result.addAll((elements.indices).map {
+//                doubling.slice(it until it + i).sum()
+//            })
+//        }
+//        return result.size
+//    }
+//}
